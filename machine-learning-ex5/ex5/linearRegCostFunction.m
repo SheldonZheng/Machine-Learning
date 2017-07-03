138110a5b4f49bf5beda5df53ed4af65730e3124
SheldonZheng <zhenghangtxdyr@gmail.com>
2017-07-03 11:19:32 +0800

    Regularized linear regression gradient

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,8 +19,9 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+h = X * theta;
 
-J = 1 / (2 * m) * sum(((X * theta) - y) .^ 2);
+J = 1 / (2 * m) * sum((h - y) .^ 2);
 
 theta1 = [0;theta(2:end)];
 
@@ -30,9 +31,8 @@ Reg = lambda * sum(theta1 .^ 2) / (2 * m);
 J = J + Reg;
 
 
-
 % =========================================================================
 
-grad = grad(:);
+grad = (X' * (h-y) + lambda * theta1) / m;
 
 end
